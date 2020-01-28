0f34f6909ee49d2e819e0d5e81cdb75321244e51
Colin Brayton <gringolalia@gmail.com>
2020-01-28 16:01:42 -0300

    singular sensation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,36 @@
-<template lang="pug">
+	<template lang="pug">
 v-container(grid-list-xl text-xs-center)
 	v-slide-y-transition(mode='out-in')
 		v-layout(row wrap)
 			v-flex
-				h1.display-2.with.dignity A Press Passage
-				img.my-5(src='@/assets/biohazard.svg' alt='Adeus, Mundo Imundo' width='175px')
+				h1.display-1 A Press Passage
+				img.my-3(src='@/assets/biohazard.svg' alt='Adeus, Mundo Imundo' width='175px')
 
-				blockquote.dignity(style='font-size: 20px; max-width: 450px; margin: 0 auto;' )
+				blockquote.dignity(style='font-size: 18px; max-width: 450px; margin: 0 auto;')
 					| With the development of the printing press, not only could text be mass-produced quickly, it could also be mass-produced quickly and incorrectly.
 					br
 					small
 						| &mdash;&nbsp;The Bureau Chiefs
-				v-btn.mt-3(color="blue-grey white--text") Imprints
-				v-btn.mt-3(color="black white--text") Offprints
+				v-btn.v-btn-lg(color="blue-grey" dark large) Imprints
+				v-btn.mt-3(color="secondary" dark large) Offprints
 
 			v-flex
-				h1.display-2.with.dignity A Press Passage
-				img.my-5(src='@/assets/biohazard.svg' alt='Adeus, Mundo Imundo' width='175px')
+				h1.display-1 Formats to Read From
+				img.my-3(src='@/assets/biohazard.svg' alt='Adeus, Mundo Imundo' width='175px')
 
-				blockquote.dignity(style='font-size: 20px; max-width: 450px; margin: 0 auto;' )
+				blockquote(style='font-size: 18px; max-width: 450px; margin: 0 auto;' )
 					| With the development of the printing press, not only could text be mass-produced quickly, it could also be mass-produced quickly and incorrectly.
 					br
 					small
 						| &mdash;&nbsp;The Bureau Chiefs
-				v-btn.mt-3(color="blue-grey white--text") Imprints
-				v-btn.mt-3(color="grey white--text") Offprints
+				v-btn.mt-3(color="blue-grey white--text" large) Imprints
+				v-btn.mt-3(color="grey white--text" large) Offprints
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-	font-weight: normal;
+	font-weight: 300;
+	font-style: normal !important;
 }
 ul {
 	list-style-type: none;
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#607d8b',
+        secondary: '#374949',
+        accent: '#6699cc',
+        error: '#890000',
+      },
+    },
+  },
+})
